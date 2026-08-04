b52d74a9f599863ae819553c3e6d0f3b2bd90535
Sasha Chernyakov <sashachernyakov111111@gmail.com>
2026-08-04 20:26:12 +0400

    Initial commit

---
+++ dist/index.js
@@ -0,0 +1,77 @@
+const corp_select = document.getElementById("corp_select");
+const table = document.getElementById("offers");
+const corps = await (await fetch("https://esi.evetech.net/corporations/npccorps")).json();
+/** @type {Array} */
+const prices_raw = await (await fetch("https://esi.evetech.net/markets/prices")).json();
+const prices = new Map();
+prices_raw.forEach(rawPrice => {
+	prices[rawPrice.type_id] = rawPrice.average_price;
+});
+
+corps.forEach(corp_id => {
+	const corp_option = document.createElement("option");
+	corp_option.innerText = corp_id;
+	corp_select.appendChild(corp_option);
+});
+
+/**
+ * @typedef {Object} Offer
+ * @property {Array} required_items
+ */
+
+/**
+ * @param {Offer} offer
+ */
+function get_isk_per_lp(offer) {
+	const result_value = prices[offer.type_id] * offer.quantity;
+	let required_items_value = 0;
+	offer.required_items.forEach(required_item => {
+		const item_value = prices[required_item.type_id] * required_item.quantity;
+		required_items_value += item_value;
+	});
+	const isk_profit = result_value - required_items_value - offer.isk_cost;
+	const isk_per_lp = isk_profit / offer.lp_cost;
+	return isk_per_lp;
+}
+
+corp_select.addEventListener("change", async e => {
+	table.innerHTML = "";
+	const corporation_id = e.target.value;
+
+	/** @type {[Offer]} */
+	const offers = await (await fetch(`https://esi.evetech.net/loyalty/stores/${corporation_id}/offers`)).json();
+
+	offers.sort((a, b) => get_isk_per_lp(b) - get_isk_per_lp(a));
+	offers.forEach(offer => {
+		let row = document.createElement("tr");
+		let rowspan = Math.max(offer.required_items.length, 1);
+		function insertCell(text) {
+			const cell = row.insertCell();
+			cell.innerText = text;
+			cell.rowSpan = rowspan;
+		}
+		insertCell(offer.type_id);
+		insertCell(offer.quantity);
+		insertCell(offer.lp_cost);
+		insertCell(offer.isk_cost);
+
+		if (offer.required_items.length == 0) {
+			row.insertCell();
+			row.insertCell();
+			insertCell(get_isk_per_lp(offer));
+		}
+		offer.required_items.forEach((required_item, i) => {
+			if (i > 0) {
+				table.appendChild(row);
+				row = document.createElement("tr");
+			}
+			row.insertCell().innerText = required_item.type_id;
+			row.insertCell().innerText = required_item.quantity;
+			if (i == 0) {
+				insertCell(get_isk_per_lp(offer));
+			}
+		});
+		table.appendChild(row);
+	});
+});
+
---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import js from "@eslint/js";
+import globals from "globals";
+import css from "@eslint/css";
+import { defineConfig } from "eslint/config";
+import stylistic from "@stylistic/eslint-plugin";
+
+export default defineConfig([
+	{ files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+	{ files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+	{
+		plugins: {
+			"@stylistic": stylistic
+		},
+		rules: {
+			"@stylistic/indent": ["warn", "tab"],
+			"@stylistic/semi": "warn",
+			"@stylistic/quotes": "warn"
+		}
+	}
+]);
